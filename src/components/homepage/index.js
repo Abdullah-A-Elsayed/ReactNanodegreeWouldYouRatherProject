daa5a3f1808d6063c90ef553d8021b429b1666df
Abdullah <abdullah.ali26@hotmail.com>
2020-05-08 21:23:05 +0200

    feat:sorting polls in homepage

--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -27,12 +27,14 @@ function HomePage({ answeredPollsIds, UnansweredPollsIds }) {
 }
 const mapStateToProps = (state) => {
   const userObject = state.users[state.authedUser];
-  const answeredPollsIds = Object.keys(userObject.answers);
+  const answeredPollsIds = Object.keys(userObject.answers).sort(
+    (a, b) => state.polls[b].timestamp - state.polls[a].timestamp
+  );
   return {
     answeredPollsIds,
-    UnansweredPollsIds: Object.keys(state.polls).filter(
-      (k) => answeredPollsIds.indexOf(k) === -1
-    ),
+    UnansweredPollsIds: Object.keys(state.polls)
+      .filter((k) => answeredPollsIds.indexOf(k) === -1)
+      .sort((a, b) => state.polls[b].timestamp - state.polls[a].timestamp),
   };
 };
 export default connect(mapStateToProps)(HomePage);
